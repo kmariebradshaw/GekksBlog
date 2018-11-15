e67d154e2cc5eede064ab25a2a0a6282714c3d8a
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-11-15 16:23:16 -0600

    adds summary to the blog posts and adds tag gem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,6 @@ class ArticlesController < ApplicationController
   end 
   private
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :text, :summary)
   end
 end
