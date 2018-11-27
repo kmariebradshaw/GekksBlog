58386fcfa4e81e4a303ea6c51a827f7f879cbcf1
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-11-27 15:28:56 -0600

    comments will delete with associated article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   mount_uploader :featured_image, ImageUploader
     validates :title, presence: true
     acts_as_taggable # Alias for acts_as_taggable_on :tags
