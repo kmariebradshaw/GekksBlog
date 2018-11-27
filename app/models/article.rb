3a5f69d27d7b95c3ff44f241889852f4a9d4dc96
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-11-27 15:19:46 -0600

    ability to create a commment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,5 @@
 class Article < ApplicationRecord
-    has_many :comments
-
+  has_many :comments
   mount_uploader :featured_image, ImageUploader
     validates :title, presence: true
     acts_as_taggable # Alias for acts_as_taggable_on :tags
