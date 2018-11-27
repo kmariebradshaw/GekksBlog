49c8c0cfd16103110212c26b50403da797dd5e51
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-11-27 16:42:21 -0600

    updates the gemfile to a new version of the taggable that doesn't have the bug I was encountering

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ApplicationRecord
+  acts_as_taggable # Alias for acts_as_taggable_on :tags
+  acts_as_taggable_on :fashion, :tech, :staff, :diy, :product
+
+
   has_many :comments, dependent: :destroy
   mount_uploader :featured_image, ImageUploader
     validates :title, presence: true
-    acts_as_taggable # Alias for acts_as_taggable_on :tags
 end
