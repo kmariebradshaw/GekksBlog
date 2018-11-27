166c723dc471691fb56f171d5a009b1af544db4f
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-11-27 15:15:01 -0600

    adds comment migration and model associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+    has_many :comments
+
   mount_uploader :featured_image, ImageUploader
     validates :title, presence: true
     acts_as_taggable # Alias for acts_as_taggable_on :tags
