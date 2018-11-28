d5ebe4d36e1a1030b1b34a16d882c87b333b773d
Kaitlyn Bradshaw <kaitlynbradshaw@Blairs-MacBook-Pro-2.local>
2018-11-28 14:38:05 -0600

    check boxes to add tags and I finally figured out the gem after much fiddling so hence all the files I touched

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,5 @@
 class Article < ApplicationRecord
   acts_as_taggable # Alias for acts_as_taggable_on :tags
-  acts_as_taggable_on :fashion, :tech, :staff, :diy, :product
 
 
   has_many :comments, dependent: :destroy
